841f6c4d3c053ba1b7c48f56787f81e482c27192
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-02-18 17:04:35 -0500

    Update config.h

--- MM-control-01/config.h
+++ MM-control-01/config.h
@@ -123,7 +123,7 @@
 #define STA_ERR_TMC2_PIN_ENA  -26 //TMC2130 axis2 pin wirring error - ena signal
 
 //number of extruders
-#define EXTRUDERS 16
+#define EXTRUDERS 10
 
 //diagnostic functions
 //#define _DIAG
